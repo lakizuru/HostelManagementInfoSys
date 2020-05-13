be99caa8867343f73aed20f5c613e17aab5a5c9e
KavinduBim <63743812+KavinduBim@users.noreply.github.com>
2020-05-13 22:09:04 +0530

    Create Notification.java

---
+++ src/model/Notification.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Perera A.A.K.B. IT19080154
+ */
+public class Notification {
+    
+    private String notifUsername;
+    private String notifTitle;
+    private String notifDate;
+    private String notifDescription;
+
+    public Notification() {
+    }
+    
+    
+
+    public Notification(String notifUsername, String notifTitle, String notifDate, String notifDescription) {
+        this.notifUsername = notifUsername;
+        this.notifTitle = notifTitle;
+        this.notifDate = notifDate;
+        this.notifDescription = notifDescription;
+    }
+
+    public String getNotifUsername() {
+        return notifUsername;
+    }
+
+    public void setNotifUsername(String notifUsername) {
+        this.notifUsername = notifUsername;
+    }
+
+    public String getNotifTitle() {
+        return notifTitle;
+    }
+
+    public void setNotifTitle(String notifTitle) {
+        this.notifTitle = notifTitle;
+    }
+
+    public String getNotifDate() {
+        return notifDate;
+    }
+
+    public void setNotifDate(String notifDate) {
+        this.notifDate = notifDate;
+    }
+
+    public String getNotifDescription() {
+        return notifDescription;
+    }
+
+    public void setNotifDescription(String notifDescription) {
+        this.notifDescription = notifDescription;
+    }
+    
+    
+    
+    
+}
